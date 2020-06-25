c01fda9a68ad6bd701093303e277cd4116158ed0
StewieButthurt <nosoff.slawa@yandex.ru>
2020-06-26 02:30:45 +0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,56 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container">
+      <div class="title">
+        Table UI
+      </div>
+      <div class="border">
+        <app-table-components />
+      </div>
     </div>
-    <router-view/>
   </div>
+    
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+const AppTableComponents = () => import('./components/Table/index.vue')
+export default {
+  name: 'app',
+  data () {
+    return {
+      
     }
+  },
+  components: {
+    AppTableComponents
   }
 }
+</script>
+
+<style lang="sass">
+  @import "~normalize.css"
+  @import "./assets/fonts/source-sans-pro.css"
+
+  #app
+    width: 100%
+    display: flex
+
+  .container
+    min-width: 1366px
+    min-height: 600px
+    margin: 0 auto
+    padding-top: 32px
+    padding-left: 113px
+    padding-right: 113px
+  
+  .title
+    font-size: 32px
+    font-family: 'Source Sans Pro SemiBold', sans-serif;
+  
+  .border
+    height: 1px
+    background-color: #EDEDED
+    margin-top: 16px
+
 </style>
---
+++ src/components/Table/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        test
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('hi')
+        }
+    }
+</script>
+
+<style lang="sass">
+
+</style>
