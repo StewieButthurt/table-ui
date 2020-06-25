e5979c724ea9244ef6425eb7018d6b04f6e7f630
StewieButthurt <nosoff.slawa@yandex.ru>
2020-06-26 03:28:48 +0400

    Добавление контейнера будущей таблицы

---
+++ src/components/Table-Components/index.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+
+        <div class="title">
+            Table UI
+        </div>
+
+        <div class="border" />
+
+        <!-- <app-filters /> -->
+
+        
+    </div>
+</template>
+
+<script>
+    // const AppFilters = () => import('./filters/index.vue')
+    export default {
+        // components: {
+        //     AppFilters
+        // }
+    }
+</script>
+
+<style lang="sass">
+
+    .container
+        min-width: 1366px
+        min-height: 600px
+        margin: 0 auto
+        padding-top: 32px
+        padding-left: 113px
+        padding-right: 113px
+
+    .title
+        font-size: 32px
+        font-family: 'Source Sans Pro SemiBold', sans-serif
+
+    .border
+        height: 1px
+        background-color: #EDEDED
+        margin-top: 16px
+
+</style>
